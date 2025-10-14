f1cecb43260b1d30a0b6c668d73c8e5b7145fe71
That1DireWolf-4746 <vwgchan@gmail.com>
2025-10-13 20:06:50 -0700

    Update SpellAction.kt

--- Common/src/main/java/at/petrak/hexcasting/api/casting/castables/SpellAction.kt
+++ Common/src/main/java/at/petrak/hexcasting/api/casting/castables/SpellAction.kt
@@ -8,7 +8,10 @@ import at.petrak.hexcasting.api.casting.eval.sideeffects.OperatorSideEffect
 import at.petrak.hexcasting.api.casting.eval.vm.CastingImage
 import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation
 import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.casting.iota.EntityIota
+import at.petrak.hexcasting.api.mod.HexTags
 import at.petrak.hexcasting.api.casting.mishaps.MishapNotEnoughArgs
+import at.petrak.hexcasting.api.casting.mishaps.MishapImmuneEntity
 import at.petrak.hexcasting.api.casting.mishaps.MishapNotEnoughMedia
 import at.petrak.hexcasting.common.lib.hex.HexEvalSounds
 import at.petrak.hexcasting.api.casting.mishaps.Mishap
