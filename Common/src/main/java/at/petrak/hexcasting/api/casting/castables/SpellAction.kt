62455a30682bbbc9eb673f85220e77fba00872d4
That1DireWolf-4746 <vwgchan@gmail.com>
2025-10-13 20:22:31 -0700

    Update SpellAction.kt

--- Common/src/main/java/at/petrak/hexcasting/api/casting/castables/SpellAction.kt
+++ Common/src/main/java/at/petrak/hexcasting/api/casting/castables/SpellAction.kt
@@ -17,6 +17,7 @@ import at.petrak.hexcasting.api.casting.mishaps.MishapNotEnoughMedia
 import at.petrak.hexcasting.common.lib.hex.HexEvalSounds
 import at.petrak.hexcasting.api.casting.mishaps.Mishap
 import net.minecraft.world.entity.Entity
+import net.minecraft.server.level.TicketType
 import net.minecraft.nbt.CompoundTag
 
 interface SpellAction : Action {
